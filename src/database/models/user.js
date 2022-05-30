ca4cef04093ca7181daf9000205a046b9a202676
Juliane Alves <julianeju3654@gmail.com>
2022-05-30 14:35:21 -0300

    Requisito 02

---
+++ src/database/models/user.js
@@ -0,0 +1,18 @@
+// segue a ordem do Readme
+const User = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: DataTypes.INTEGER, 
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+      }, {
+      timestamps: false
+    })
+    return User;
+};
+
+module.exports = User
+
+
+  
